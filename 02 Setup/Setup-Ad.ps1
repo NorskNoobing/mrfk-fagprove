08ad21bc19db66e0ac1aa2683d217adb49498ecb
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-01-11 12:27:00 +0100

    Change script to be compatible with module

--- 02 Setup/Setup-Ad.ps1
+++ 02 Setup/Setup-Ad.ps1
@@ -9,76 +9,23 @@ param(
     $AdminsOuPath = "OU=Admins,$BaseOuPath"
 )
 
-function New-MrfkAdUser {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)][string]$DisplayName,
-        [Parameter(Mandatory)][ValidateSet("Standard","Admin")][string]$UserType,
-        [Parameter(Mandatory)][string]$Path
-    )
-
-    process {
-        $NameSplit = ($DisplayName -split " ")
-    
-        [string]$GivenName = $NameSplit[0..$NameSplit.IndexOf($NameSplit[-2])]
-        $Surname = $NameSplit[-1]
-
-        #Set username
-        switch ($UserType) {
-            Standard {
-                $UsernameStr = $GivenName.Substring(0,3) + $Surname.Substring(0,3)
-            }
-            Admin {
-                $UsernameStr = "adm_" + $GivenName.Substring(0,3) + $Surname.Substring(0,3)
-                $DisplayName = "$DisplayName (Admin)"
-            }
-        }
-
-        $UsernameStr = $UsernameStr.ToLower()
-        $StartNum = "1"
-
-        #Auto inrement username if it's already taken
-        while (!$ExitLoop) {
-            $Username = $UsernameStr + $StartNum.PadLeft(2,"0")
-            try {
-                $UserExists = Get-ADUser $Username
-            }
-            catch {}
-
-            if (!$UserExists) {
-                $ExitLoop = $true
-            } else {
-                $StartNum++
-            }
-        }
-        
-        $splat = @{
-            "AccountPassword" = Read-Host "Please enter a new password for user `"$Username`"" -AsSecureString
-            "DisplayName" = $DisplayName
-            "Enabled" = $true
-            "GivenName" = $GivenName
-            "Surname" = $Surname
-            "SamAccountName" = $Username
-            "Name" = $Username
-            "Path" = $Path
-        }
-        New-ADUser @splat
-    }
-}
-
 #Create OUs
 New-ADOrganizationalUnit -Name "CORP" -Path $DomainRootPath
 New-ADOrganizationalUnit -Name "Admins" -Path $BaseOuPath
 New-ADOrganizationalUnit -Name "Users" -Path $BaseOuPath
 New-ADOrganizationalUnit -Name "Groups" -Path $BaseOuPath
 
+#Import module with function to create users
+$ModuleRoot = (Get-Item $PSScriptRoot).parent.fullname
+Import-Module "$ModuleRoot\NN.Fagprove\NN.Fagprove\0.0.1\NN.Fagprove.psm1"
+
 #Create all users
 $AdminsArr.ForEach({
-    New-MrfkAdUser -DisplayName $_ -UserType "Admin" -Path $AdminsOuPath
+    New-FpAdUser -DisplayName $_ -UserType "Admin" -Path $AdminsOuPath
 })
 
 $UsersArr.ForEach({
-    New-MrfkAdUser -DisplayName $_ -UserType "Standard" -Path $UsersOuPath
+    New-FpAdUser -DisplayName $_ -UserType "Standard" -Path $UsersOuPath
 })
 
 #Create AD-groups
