f3f738a6d225aa8f842b68d0ca26b49f05dddf60
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-01-11 21:27:05 +0100

    import function into module

--- Setup/test.ps1
+++ Setup/test.ps1
@@ -1,19 +1,7 @@
-function New-AdStructure {
-    param (
-        [Parameter(Mandatory)][xml]$Node,
-        [string]$Path
-    )
-
-    $DomainRoot = (Get-ADDomain).DistinguishedName
-    if ($Node.OU) {
-        New-ADOrganizationalUnit -Path $DomainRoot -Name $Node.OU.Name
-    } elseif ($Node.Group) {
-
-    }
-
-    if ($Node.object) {
-        New-AdStructure -Node $Node.object -Path ($Path + <#todo: Insert AD DN path of OU #>)
-    }
-}
+#Import module with function to create users
+$RepoRoot = (Get-Item $PSScriptRoot).parent.fullname
+Import-Module "$RepoRoot\NN.Fagprove\NN.Fagprove\0.0.1\NN.Fagprove.psm1"
 
 [xml]$xml = Get-Content -Raw "$PSScriptRoot\AD-structure.xml"
+$DomainRoot = (Get-ADDomain).DistinguishedName
+New-AdStructure -Node $xml.object -Path $DomainRoot
